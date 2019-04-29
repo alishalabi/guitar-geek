e008d9f8b5a7ae69670306fa36cbc4f5e9dc455c
Ali Shalabi <alishalabi@Alis-MacBook-Pro-2.local>
2019-04-28 17:00:45 -0700

    Can fully CRUD songs

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs = require("express-handlebars")
 const mongoose = require("mongoose")
 const pitchy = require("pitchy")
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 
 // Instantiate Express (Must be after middleware requirements)
@@ -14,6 +15,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/guitar-geek', { useNewUrlParser: true });
 
 
@@ -71,8 +73,7 @@ app.get('/songs', (req, res) => {
   Song.find()
     .then(songs => {
       res.render('songs-index', { songs: songs });
-    })
-    .catch(err => {
+    }).catch(err => {
       console.log(err);
     })
 })
@@ -86,6 +87,43 @@ app.get('/songs/new', (req, res) => {
 app.post('/songs', (req, res) => {
   Song.create(req.body).then((song) => {
     console.log(song);
+    res.redirect(`/songs/${song._id}`);
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
+// HTTP: Show One Song
+app.get('/songs/:id', (req, res) => {
+  Song.findById(req.params.id)
+    .then((song) => {
+      res.render('songs-show', {song: song})
+    }).catch((err) => {
+      console.log(err.message)
+    })
+})
+
+// HTTP: Edit Song Form
+app.get('/songs/:id/edit', (req, res) => {
+  Song.findById(req.params.id, function(err, song) {
+    res.render('songs-edit', {song: song});
+  })
+})
+
+// HTTP: Update Song
+app.put('/songs/:id', (req, res) => {
+  Song.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/songs/${song._id}`)
+    }).catch(err => {
+      console.log(err.message)
+    })
+})
+
+// HTTP: Delete Song
+app.delete('/songs/:id', function (req, res) {
+  console.log("DELETE song")
+  Song.findByIdAndRemove(req.params.id).then((song) => {
     res.redirect('/songs');
   }).catch((err) => {
     console.log(err.message);
